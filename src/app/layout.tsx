c0072d6a7d042880358a12bb8ab865edb881487b
Huy Do Quang <huy.do.quang@advesa.com>
2024-10-07 16:36:16 +0700

    dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from 'next/link'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,28 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex h-screen bg-gray-100">
+          <div className="hidden md:flex flex-col w-64 bg-gray-800">
+            <div className="flex items-center justify-center h-16 bg-gray-900">
+              <span className="text-white font-bold uppercase">Demo</span>
+            </div>
+            <div className="flex flex-col flex-1 overflow-y-auto">
+              <nav className="flex-1 px-2 py-4 bg-gray-800">
+              <Link href="/home"  className="flex items-center px-4 py-2 mt-2 text-gray-100 hover:bg-gray-700">
+                Shopee Parse
+              </Link>
+              <Link href="/simple"  className="flex items-center px-4 py-2 mt-2 text-gray-100 hover:bg-gray-700">
+                Simple List
+              </Link>
+              </nav>
+            </div>
+          </div>
+          <div className="flex flex-col flex-1 overflow-y-auto">
+              <div className="p-4">     
+                {children}
+              </div>
+          </div>
+        </div>
       </body>
     </html>
   );
---
+++ src/app/simple/page.tsx
@@ -0,0 +1,153 @@
+"use client";
+
+import ExcelExport from "export-xlsx";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import URL from "url-parse";
+
+const SETTINGS_FOR_EXPORT = {
+  fileName: "example",
+  workSheets: [
+    {
+      sheetName: "example",
+      startingRowNumber: 2,
+      tableSettings: {
+        table1: {
+          tableTitle: "Shopee Check",
+          headerDefinition: [
+            {
+              name: "Name",
+              key: "name",
+            },
+            {
+              name: "Origin URL",
+              key: "url",
+            },
+            {
+              name: "Product Link",
+              key: "product_link",
+            },
+            {
+              name: "Shop Id",
+              key: "idx",
+            },
+            {
+              name: "Product Id",
+              key: "ext",
+            },
+          ],
+        },
+      },
+    },
+  ],
+};
+
+type XItem = {
+  url: string;
+  name: string;
+  idx: string;
+  ext: string;
+  product_link: string;
+};
+
+const Home = () => {
+  const [list, setList] = useState<XItem[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleURL = () => {
+    if (!searchTerm.trim()) return;
+    const url = new URL(searchTerm);
+    const paths = url.pathname.split(".");
+
+    const newItem = {
+      url: url.href,
+      name: decodeURIComponent(paths.length > 0 ? paths[0] : "").replace(
+        "/",
+        ""
+      ),
+      idx: paths.length >= 3 ? paths[paths.length - 2] : "",
+      ext: paths.length >= 3 ? paths[paths.length - 1] : "",
+    } as XItem;
+
+    newItem.product_link = `https://shopee.vn/product/${newItem.idx}/${newItem.ext}`;
+
+    setList(list.concat(newItem));
+    setSearchTerm("");
+  };
+
+  const handleClear = () => {
+    setSearchTerm("");
+  };
+
+  const handleExport = () => {
+    const data = [
+      {
+        table1: list,
+      },
+    ];
+
+    const excelExport = new ExcelExport();
+    excelExport.downloadExcel(SETTINGS_FOR_EXPORT, data);
+  };
+
+  return (
+    <>
+      <h4>SIMPLE</h4>
+      <div className="flex flex-col items-center p-4">
+        <div className="w-full max-w-md flex">
+          <Input
+            type="text"
+            placeholder="Input a URL..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <Button className="ml-2" onClick={handleURL}>
+            Add
+          </Button>
+          <Button className="ml-2" onClick={handleClear}>
+            Clear
+          </Button>
+          <Button className="ml-2" onClick={handleExport}>
+            Export Excel
+          </Button>
+        </div>
+        <div className="w-full max-w-4xl mt-8">
+          <table className="min-w-full bg-white border border-gray-200">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 border-b">Origin URL</th>
+                <th className="px-4 py-2 border-b">Parse URL</th>
+                <th className="px-4 py-2 border-b">Id Shop</th>
+                <th className="px-4 py-2 border-b">ID San Pham</th>
+              </tr>
+            </thead>
+            <tbody>
+              {list.map((item, index) => (
+                <tr key={index}>
+                  <td className="px-4 py-2 border-b">
+                    <a href={item.url} target="_blank" rel="noreferrer">
+                      {item.name}
+                    </a>
+                  </td>
+                  <td className="px-4 py-2 border-b">
+                    <a
+                      href={item.product_link}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {item.product_link}
+                    </a>
+                  </td>
+                  <td className="px-4 py-2 border-b">{item.idx}</td>
+                  <td className="px-4 py-2 border-b">{item.ext}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
